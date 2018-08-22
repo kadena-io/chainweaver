794e27203cda2d0e3bfce616c803ce0cd617bc9f
Robert Klotzner <robert.klotzner@gmx.at>
2018-08-22 20:57:28 +0200

    Moved index.css to the bottom.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -42,13 +42,13 @@ frontend = (head', body)
       elAttr "link" ("href" =: "http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" <> "rel" =: "stylesheet") blank
       elAttr "link" ("href" =: static @"semantic-reflex/semantic.min.css" <> "rel" =: "stylesheet") blank
       elAttr "link" ("href" =: static @"css/font-awesome.min.css" <> "rel" =: "stylesheet") blank
-      elAttr "link" ("href" =: static @"css/index.css" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js") blank
       elAttr "script" ("src" =: "http://code.jquery.com/ui/1.12.1/jquery-ui.js") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "http://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js" <> "charset" =: "utf-8") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"js/ace-mode-pact.js") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "http://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/theme-solarized_light.js") blank
+      elAttr "link" ("href" =: static @"css/index.css" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
     body = app
       {- text "Welcome to Obelisk!" -}
       {- el "p" $ text $ T.pack commonStuff -}
