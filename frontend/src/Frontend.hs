0cc288df798e6f8c7225eb0b5096051cb2c08721
Robert Klotzner <robert.klotzner@gmx.at>
2018-08-28 23:46:01 +0200

    Changed http uris to https uris.

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -38,16 +38,16 @@ frontend = (head', body)
       elAttr "meta" ("content" =: "#ffffff" <> "name" =: "msapplication-TileColor") blank
       elAttr "meta" ("content" =: "ms-icon-144x144.png" <> "name" =: "msapplication-TileImage") blank
       elAttr "meta" ("content" =: "#ffffff" <> "name" =: "theme-color") blank
-      elAttr "link" ("href" =: "http://fonts.googleapis.com/css?family=Roboto:300,600" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
-      elAttr "link" ("href" =: "http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" <> "rel" =: "stylesheet") blank
+      elAttr "link" ("href" =: "https://fonts.googleapis.com/css?family=Roboto:300,600" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
+      elAttr "link" ("href" =: "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" <> "rel" =: "stylesheet") blank
       elAttr "link" ("href" =: static @"semantic-reflex/semantic.min.css" <> "rel" =: "stylesheet") blank
       elAttr "link" ("href" =: static @"css/font-awesome.min.css" <> "rel" =: "stylesheet") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js") blank
-      elAttr "script" ("src" =: "http://code.jquery.com/ui/1.12.1/jquery-ui.js") blank
-      elAttr "script" ("type" =: "text/javascript" <> "src" =: "http://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js" <> "charset" =: "utf-8") blank
+      elAttr "script" ("src" =: "https://code.jquery.com/ui/1.12.1/jquery-ui.js") blank
+      elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js" <> "charset" =: "utf-8") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"js/ace-mode-pact.js") blank
-      elAttr "script" ("type" =: "text/javascript" <> "src" =: "http://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/theme-solarized_light.js") blank
+      elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/theme-solarized_light.js") blank
       elAttr "link" ("href" =: static @"css/index.css" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"css-element-queries-1.0.2/src/ResizeSensor.js") blank
     body = app
