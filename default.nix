3a66af5928b4c94fd1587f2852c2a5373dbd8208
Robert Klotzner <robert.klotzner@gmx.at>
2018-10-05 18:55:51 +0200

    Test commit for making the pact backend work.

--- default.nix
+++ default.nix
@@ -172,8 +172,8 @@ let
 
             proxy_pass http://127.0.0.1:7010;
             proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
+            # proxy_set_header Upgrade $http_upgrade;
+            # proxy_set_header Connection "upgrade";
           }
         }
     '';
