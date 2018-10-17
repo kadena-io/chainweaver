46733c5a3454be9291e4c6579f74c13ab7edbfac
Robert Klotzner <robert.klotzner@gmx.at>
2018-10-17 17:36:11 +0200

    Handle missing config file case properly.

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Backend where
 
+import           Control.Exception               (catchJust)
 import           Control.Monad                   (void)
 import           Control.Monad.IO.Class          (liftIO)
+import           Data.Foldable                   (traverse_)
 import           Data.Monoid                     ((<>))
 import qualified Data.Text                       as T
 import qualified Data.Text.IO                    as T
 import           Obelisk.ExecutableConfig.Inject (injectPure)
+import           System.IO.Error                 (isDoesNotExistError)
 
 import           Common.Api
 import           Frontend
@@ -17,7 +21,10 @@ backend = Ob.backend Ob.def
       fst frontend
       -- TODO: When upgrading Obelisk, we should use that:
       -- injectExecutableConfigs
-      let k = "common/server-url"
-      url <- liftIO $ T.readFile ("config/" <> k)
-      injectPure k url
+      let
+        k = "common/server-url"
+        doesNotExist = \e -> if isDoesNotExistError e then Just () else Nothing
+      mUrl <- liftIO $ catchJust doesNotExist
+	(fmap Just . T.readFile $ "config/" <> k) (const $ pure Nothing)
+      traverse_ (injectPure k) mUrl
   }
