e48a63f5f83a1f97bf49eeeedb5317e001065ca4
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2018-10-18 18:37:45 -0400

    Use new frontend and backend definitions

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -6,11 +6,14 @@ module Frontend where
 import qualified Data.Text as T
 import           Reflex.Dom.Core
 
+import           Common.Route
 import           Frontend.ReplGhcjs
+import           Obelisk.Frontend
+import           Obelisk.Route
 import           Obelisk.Generated.Static
 
-frontend :: (StaticWidget x (), Widget x ())
-frontend = (head', body)
+frontend :: Frontend (R FrontendRoute)
+frontend = Frontend { _frontend_head = head', _frontend_body = app }
   where
     head' = do
       -- Global site tag (gtag.js) - Google Analytics
@@ -41,4 +44,3 @@ frontend = (head', body)
       elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"js/nacl-fast.min-v1.0.0.js") blank
       elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/theme-solarized_dark.js") blank
       elAttr "link" ("href" =: static @"css/index.css" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
-    body = app
