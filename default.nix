9f53dea2b5386e0a30655f38bc38945d16ae86c4
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2018-10-18 11:05:22 -0400

    Changes for new reflex-platform

--- default.nix
+++ default.nix
@@ -14,73 +14,48 @@ let
     overrides = self: super:
       let guardGhcjs = p: if self.ghc.isGhcjs or false then null else p;
 
-          algebraic-graphs = pkgs.fetchFromGitHub {
-            owner = "snowleopard";
-            repo = "alga";
-            rev = "1c04f5664b9476e0b01a573b40462531e52e8756";
-            sha256 = "0j121551zqjrp4xy0qcz1pk46znr6w59jkg75v5svdh9ag3vmbsp";
-          };
-          blake2 = pkgs.fetchFromGitHub {
-            owner = "kadena-io";
-            repo = "blake2";
-            rev = "6c106bcf0f3e85448cec08353e4e7d34e086e296";
-            sha256 = "0pjfhy4sf4qqz0h2b658r4qy92qx82cssvdh1wkrrimzk9q1icvi";
-          };
           semantic-reflex-src = pkgs.fetchFromGitHub {
             owner = "tomsmalley";
             repo = "semantic-reflex";
-            rev = "38fce7e4d08d46b8664768f1b7fe38846dbac1e2";
-            sha256 = "1s2p12r682wd8j2z63pjvbi4s9v02crh6nz8kjilwdsfs02yp5p2";
-          };
-          megaparsec = pkgs.fetchFromGitHub {
-            owner  = "mrkkrp";
-            repo   = "megaparsec";
-            rev    = "7b271a5edc1af59fa435a705349310cfdeaaa7e9";
-            sha256 = "0415z18gl8dgms57rxzp870dpz7rcqvy008wrw5r22xw8qq0s13c";
-          };
-          modern-uri = pkgs.fetchFromGitHub {
-            owner  = "mrkkrp";
-            repo   = "modern-uri";
-            rev    = "7c02e3d708aa75bcaf050d151281190e33170143";
-            sha256 = "0sc8b5787v7bgziqqvcl0qp0ysls12lqqvpv4dnm01gl69b52yyi";
+            rev = "42bfede5e308bab4494e87ed0144f21134a4c5b3";
+            sha256 = "01rpf0vh5llx1hq4j55gmw36fvzhb95ngcykh34sgcxp5498p9f3";
           };
       in {
-            algebraic-graphs = self.callCabal2nix "algebraic-graphs" algebraic-graphs {};
-            blake2 = guardGhcjs (self.callCabal2nix "blake2" blake2 {});
             cacophony = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callHackage "cacophony" "0.8.0" {}));
-            cryptonite = guardGhcjs (self.callHackage "cryptonite" "0.23" {});
             haskeline = guardGhcjs (self.callHackage "haskeline" "0.7.4.2" {});
             katip = guardGhcjs (pkgs.haskell.lib.doJailbreak (self.callHackage "katip" "0.3.1.4" {}));
             ridley = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callHackage "ridley" "0.3.1.2" {}));
 
-            # Needed to work with the below version of statistics
-            criterion = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "criterion" (pkgs.fetchFromGitHub {
-              owner = "bos";
-              repo = "criterion";
-              rev = "5a704392b670c189475649c32d05eeca9370d340";
-              sha256 = "1kp0l78l14w0mmva1gs9g30zdfjx4jkl5avl6a3vbww3q50if8pv";
-            }) {}));
+            bound = pkgs.haskell.lib.dontCheck super.bound;
+            bytes = pkgs.haskell.lib.dontCheck super.bytes;
+            # doctest = self.callHackage "doctest" "0.16.0" {};
+            extra = pkgs.haskell.lib.dontCheck super.extra;
+            io-streams = pkgs.haskell.lib.dontCheck super.io-streams;
+            lens-aeson = pkgs.haskell.lib.dontCheck super.lens-aeson;
+            trifecta = pkgs.haskell.lib.dontCheck super.trifecta;
 
-            # Version 1.6.4, needed by weeder, not in callHackage yet
-            extra = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "extra" (pkgs.fetchFromGitHub {
-              owner = "ndmitchell";
-              repo = "extra";
-              rev = "4064bfa7e48a7f1b79f791560d51dbefed879219";
-              sha256 = "1p7rc5m70rkm1ma8gnihfwyxysr0n3wxk8ijhp6qjnqp5zwifhhn";
-            }) {}));
+            # # Needed to work with the below version of statistics
+            # criterion = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "criterion" (pkgs.fetchFromGitHub {
+            #   owner = "bos";
+            #   repo = "criterion";
+            #   rev = "5a704392b670c189475649c32d05eeca9370d340";
+            #   sha256 = "1kp0l78l14w0mmva1gs9g30zdfjx4jkl5avl6a3vbww3q50if8pv";
+            # }) {}));
 
-            megaparsec = pkgs.haskell.lib.dontCheck (self.callCabal2nix "megaparsec" megaparsec {});
-
-            modern-uri = pkgs.haskell.lib.dontCheck (self.callCabal2nix "modern-uri" modern-uri {});
+            # # Version 1.6.4, needed by weeder, not in callHackage yet
+            # extra = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "extra" (pkgs.fetchFromGitHub {
+            #   owner = "ndmitchell";
+            #   repo = "extra";
+            #   rev = "4064bfa7e48a7f1b79f791560d51dbefed879219";
+            #   sha256 = "1p7rc5m70rkm1ma8gnihfwyxysr0n3wxk8ijhp6qjnqp5zwifhhn";
+            # }) {}));
 
             pact = pkgs.haskell.lib.dontCheck (self.callCabal2nix "pact" (pkgs.fetchFromGitHub {
               owner = "kadena-io";
               repo = "pact";
-              rev = "d4418c67e66e29e2627a7cd5a4fbf32db4af5cc1";
-              sha256 = "062cy72sd83wymmzxwd8gbrwh6hi9ks7m8hjbhywsrblm83gi2f3";
+              rev = "3fd9d9d470069281b0b96e802cb990a210392284";
+              sha256 = "1lihkkhx2gkld9gkyz1dfnbv1hq44bzswjv9g9fkhrjmf8xv4wav";
             }) {});
-
-            parser-combinators = self.callHackage "parser-combinators" "0.4.0" {};
 
             reflex-dom-ace = (self.callCabal2nix "reflex-dom-ace" (pkgs.fetchFromGitHub {
               owner = "reflex-frp";
@@ -98,12 +73,12 @@ let
             }) {});
 
             # dontCheck is here because a couple tests were failing
-            statistics = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "statistics" (pkgs.fetchFromGitHub {
-              owner = "bos";
-              repo = "statistics";
-              rev = "1ed1f2844c5a2209f5ea72e60df7d14d3bb7ac1a";
-              sha256 = "1jjmdhfn198pfl3k5c4826xddskqkfsxyw6l5nmwrc8ibhhnxl7p";
-            }) {}));
+            # statistics = guardGhcjs (pkgs.haskell.lib.dontCheck (self.callCabal2nix "statistics" (pkgs.fetchFromGitHub {
+            #   owner = "bos";
+            #   repo = "statistics";
+            #   rev = "1ed1f2844c5a2209f5ea72e60df7d14d3bb7ac1a";
+            #   sha256 = "1jjmdhfn198pfl3k5c4826xddskqkfsxyw6l5nmwrc8ibhhnxl7p";
+            # }) {}));
 
             thyme = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.enableCabalFlag (self.callCabal2nix "thyme" (pkgs.fetchFromGitHub {
               owner = "kadena-io";
@@ -112,7 +87,7 @@ let
               sha256 = "09fcf896bs6i71qhj5w6qbwllkv3gywnn5wfsdrcm0w1y6h8i88f";
             }) {}) "ghcjs");
 
-            semantic-reflex = pkgs.haskell.lib.dontCheck (self.callCabal2nix "semantic-reflex"  (semantic-reflex-src + /semantic-reflex) {});
+            semantic-reflex = pkgs.haskell.lib.dontHaddock (pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.dontCheck (self.callCabal2nix "semantic-reflex"  (semantic-reflex-src + /semantic-reflex) {})));
 
             # ghc-8.0.2 haddock has an annoying bug, which causes build failures:
             # See: https://github.com/haskell/haddock/issues/565
