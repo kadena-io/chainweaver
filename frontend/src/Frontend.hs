28d7180871802ea289805e9c738e3668136ead8a
Doug Beardsley <mightybyte@gmail.com>
2018-11-13 16:31:21 -0500

    Better loading indicatior and meta info for SEO

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -33,34 +33,24 @@ frontend = Frontend
         , "gtag('config', '" <> gaTrackingId <> "');"
         ]
 
-      el "title" $ text "Pact IDE"
       newHead
 
-  , _frontend_body = prerender (text "Loading, stand by...") app
+  , _frontend_body = prerender loaderMarkup app
   , _frontend_headRender = HeadRender_Static
   }
 
---oldHead :: MonadWidget t m => m ()
---oldHead = do
---    elAttr "meta" ("content" =: "#ffffff" <> "name" =: "msapplication-TileColor") blank
---    elAttr "meta" ("content" =: "ms-icon-144x144.png" <> "name" =: "msapplication-TileImage") blank
---    elAttr "meta" ("content" =: "#ffffff" <> "name" =: "theme-color") blank
---    elAttr "link" ("href" =: "https://fonts.googleapis.com/css?family=Roboto:300,600" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
---    -- elAttr "link" ("href" =: "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" <> "rel" =: "stylesheet") blank
---    elAttr "link" ("href" =: static @"semantic-reflex/semantic.min.css" <> "rel" =: "stylesheet") blank
---    elAttr "link" ("href" =: static @"css/semantic.superhero.min.css" <> "rel" =: "stylesheet") blank
---    elAttr "link" ("href" =: static @"css/font-awesome.min.css" <> "rel" =: "stylesheet") blank
---    -- elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js") blank
---    -- elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.js") blank
---    -- elAttr "script" ("src" =: "https://code.jquery.com/ui/1.12.1/jquery-ui.js") blank
---    elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/ace.js" <> "charset" =: "utf-8") blank
---    elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"js/ace-mode-pact.js") blank
---    elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"js/nacl-fast.min-v1.0.0.js") blank
---    elAttr "script" ("type" =: "text/javascript" <> "src" =: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/theme-solarized_dark.js") blank
---    elAttr "link" ("href" =: static @"css/index.css" <> "rel" =: "stylesheet" <> "type" =: "text/css") blank
+loaderMarkup :: DomBuilder t m => m ()
+loaderMarkup = do
+  divClass "spinner" $ do
+    divClass "cube1" blank
+    divClass "cube2" blank
+  divClass "spinner-msg" $ text "Loading"
 
 newHead :: DomBuilder t m => m ()
 newHead = do
+    el "title" $ text "Pact Test Net"
+    meta ("name" =: "description" <> "content" =: "Write, test, and deploy Pact smart contracts in your browser")
+    meta ("name" =: "keywords" <> "content" =: "pact, pact language, pact programming language, smart contracts, smart contract language, blockchain, cryptocurrency, learn blockchain programming, kadena, chainweb")
     meta ("charset" =: "utf-8")
     meta ("name" =: "google" <> "content" =: "notranslate")
     meta ("http-equiv" =: "Content-Language" <> "content" =: "en_US")
