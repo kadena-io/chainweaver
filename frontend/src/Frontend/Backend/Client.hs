a7b0b7ae2d943f6580c61bbca0efc6b1310760b9
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-01-24 16:19:01 -0500

    Frontend.Backend.Client: add type signatures

--- frontend/src/Frontend/Backend/Client.hs
+++ frontend/src/Frontend/Backend/Client.hs
@@ -16,20 +16,25 @@
 
 module Frontend.Backend.Client where
 
+import           Data.Aeson.Types                  (Value)
+import           Data.Proxy
 import           Control.Monad.Catch               (MonadThrow(..))
+import qualified Data.Text                         as T
+import qualified Pact.Analyze.Remote.Types as Analyze
 import           Pact.Server.API                   (pactServerAPI)
 import           Pact.Types.API
-import           Reflex.Dom.Core                   (MonadWidget)
-import           Servant
+import           Pact.Types.Command
+import           Reflex.Dom.Core hiding            (Value)
+import           Servant.API
 import qualified Servant.Client                    as SC
 import           Servant.Reflex
 
-getBaseUrl :: (MonadThrow m) => BackendUri -> m BaseUrl
+getBaseUrl :: (MonadThrow m) => T.Text -> m BaseUrl
 getBaseUrl backendUri = SC.parseBaseUrl $ T.unpack backendUri
 
 makeSend :: forall t m. (MonadWidget t m)
   => BaseUrl
-  -> Dynamic t (Either Text SubmitBatch)
+  -> Dynamic t (Either T.Text SubmitBatch)
   -> Event t ()
   -> m (Event t (ReqResult () (ApiResponse RequestKeys)))
 makeSend baseUrl = let
@@ -37,26 +42,50 @@ makeSend baseUrl = let
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
   in send
 
+makePoll :: forall t m. (MonadWidget t m)
+  => BaseUrl
+  -> Dynamic t (Either T.Text Poll)
+  -> Event t ()
+  -> m (Event t (ReqResult () (ApiResponse PollResponses)))
 makePoll baseUrl = let
   ((_ :<|> poll :<|> _ :<|> _) :<|> _ :<|> _) =
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
   in poll
 
+makeListen :: forall t m. (MonadWidget t m)
+  => BaseUrl
+  -> Dynamic t (Either T.Text ListenerRequest)
+  -> Event t ()
+  -> m (Event t (ReqResult () (ApiResponse ApiResult)))
 makeListen baseUrl = let
   ((_ :<|> _ :<|> listen :<|> _) :<|> _ :<|> _) =
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
   in listen
 
+makeLocal :: forall t m. (MonadWidget t m)
+  => BaseUrl
+  -> Dynamic t (Either T.Text (Command T.Text))
+  -> Event t ()
+  -> m (Event t (ReqResult () (ApiResponse (CommandSuccess Value))))
 makeLocal baseUrl = let
   ((_ :<|> _ :<|> _ :<|> local) :<|> _ :<|> _) =
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
   in local
 
+makeVerify :: forall t m. (MonadWidget t m)
+  => BaseUrl
+  -> Dynamic t (Either T.Text Analyze.Request)
+  -> Event t ()
+  -> m (Event t (ReqResult () Analyze.Response))
 makeVerify baseUrl = let
   ((_ :<|> _ :<|> _ :<|> _) :<|> verify :<|> _) =
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
   in verify
 
+makeVersion :: forall t m. (MonadWidget t m)
+  => BaseUrl
+  -> Event t ()
+  -> m (Event t (ReqResult () T.Text))
 makeVersion baseUrl = let
   ((_ :<|> _ :<|> _ :<|> _) :<|> _ :<|> version) =
         client pactServerAPI (Proxy :: Proxy m) (Proxy :: Proxy ()) (constDyn baseUrl)
