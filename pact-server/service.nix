d7abbf0d49aceac1050b4e5964c336bffb99424e
Doug Beardsley <mightybyte@gmail.com>
2019-01-31 00:33:43 -0500

    Add CORS headers to nginx proxy around pact server

--- pact-server/service.nix
+++ pact-server/service.nix
@@ -71,5 +71,17 @@ in {pkgs, lib, ...}: {
     };
 
   };
+
+  services.nginx.appendHttpConfig = ''
+      server {
+        location / {
+          if ($request_method = 'POST') {
+             add_header 'Access-Control-Allow-Methods' 'POST';
+             add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+             add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range';
+          }
+        }
+      }
+  '';
 }
 
