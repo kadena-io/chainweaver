6ec90b9bb9e5178fe60126095e9bb9cc1aabd52e
Robert Klotzner <robert.klotzner@gmx.at>
2019-02-16 00:08:07 +0100

    Fixed some inconsistent naming.

--- frontend/src/Frontend/UI/JsonData.hs
+++ frontend/src/Frontend/UI/JsonData.hs
@@ -218,7 +218,7 @@ uiKeysetKeys
   -> [KeyName]
   -> m (Event t (KeyName, Bool))
 uiKeysetKeys ks allKeys =
-  elClass "ul" "keyset_keys" $ do
+  elClass "ul" "keyset__keys" $ do
     case allKeys of
       []   -> do
         el "li" $ text "No keys available ..."
