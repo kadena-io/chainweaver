8a9758e629af4cd80d2499c5e9aaf619399a4fe4
Robert Klotzner <robert.klotzner@gmx.at>
2019-06-10 12:04:50 +0200

    Hopefully correct line numbers on ghcjs again. (Mostly)

--- frontend/src/Frontend/Editor.hs
+++ frontend/src/Frontend/Editor.hs
@@ -54,6 +54,8 @@ import           System.Random              (newStdGen, randoms)
 
 #ifdef  ghcjs_HOST_OS
 import           Data.Map                   (Map)
+import           Data.Bitraversable         (bitraverse)
+import           Control.Monad              (join)
 #endif
 ------------------------------------------------------------------------------
 import           Frontend.Network
@@ -221,23 +223,27 @@ typeCheckVerify m t = mdo
     pure newAnnotations
   where
 -- Line numbers are off on ghcjs:
+-- TODO: Fix this in pact.
 #ifdef  ghcjs_HOST_OS
     parseVerifyOutput :: Map ModuleName Int -> VerifyResult -> [Annotation]
     parseVerifyOutput ms rs =
       let
-        msgsRs :: [(ModuleName, Text)]
-        msgsRs = fmap (fmap $ either id id) . Map.toList $ rs
+        msgsRs :: [(ModuleName, Either Text Text)]
+        msgsRs = Map.toList $ rs
 
-        parsedRs :: Map ModuleName [Annotation]
-        parsedRs = Map.fromList $ fmapMaybe (traverse annoParser) msgsRs
+        parsedRs :: Map ModuleName (Either [Annotation] [Annotation])
+        parsedRs = Map.fromList $ mapMaybe (traverse $ join bitraverse annoParser) msgsRs
 
         fixLineNumber :: Int -> Annotation -> Annotation
         fixLineNumber n a = a { _annotation_line = _annotation_line a + n }
 
         fixLineNumbers :: Int -> [Annotation] -> [Annotation]
         fixLineNumbers n = map (fixLineNumber n)
+
+        fixLineNumbersRight :: Int -> Either [Annotation] [Annotation] -> [Annotation]
+        fixLineNumbersRight n = either id (fixLineNumbers n)
       in
-        concat . Map.elems $ Map.intersectionWith fixLineNumbers ms parsedRs
+        concat . Map.elems $ Map.intersectionWith fixLineNumbersRight ms parsedRs
 #else
     parseVerifyOutput :: VerifyResult -> [Annotation]
     parseVerifyOutput rs =
