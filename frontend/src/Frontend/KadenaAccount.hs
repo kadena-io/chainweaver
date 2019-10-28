6032f725a016323608fe86651ace30e0625e15f0
Sean Chalmers <sean.chalmers@obsidian.systems>
2019-10-29 01:30:53 +1000

    Delete key from KadenaAccount
    Add enough info so we can track back this account to the correct network and chain.

--- frontend/src/Frontend/KadenaAccount.hs
+++ frontend/src/Frontend/KadenaAccount.hs
@@ -15,14 +15,18 @@ import Pact.Types.ChainId
 
 import Kadena.SigningApi (AccountName(..), mkAccountName)
 
+import Common.Network (NetworkName)
 import Frontend.Foundation (makePactLenses)
-import Frontend.Crypto.Ed25519
 
-data KadenaAccount key = KadenaAccount
-  { _kadenaAccount_chainId :: ChainId
+data KadenaAccount = KadenaAccount
+  { _kadenaAccount_networkId :: NetworkName
+    -- ^ Blockchain specific identifier
+  , _kadenaAccount_chainId :: ChainId
+    -- ^ Platform specific identifier
   , _kadenaAccount_name :: AccountName 
-  , _kadenaAccount_key :: key
+    -- ^ Name provided for this account
   , _kadenaAccount_checksum :: Digest MD5
+    -- ^ The checksum of NetworkId and AccountName for verification
   }
   deriving Generic
 
