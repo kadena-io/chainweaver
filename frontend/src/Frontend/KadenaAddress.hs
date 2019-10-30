772135850f67d3b8095bcb88d3f8ff67d1c322b8
Sean Chalmers <sean.chalmers@obsidian.systems>
2019-10-30 22:59:03 +1000

    First implementation of the 'kadena address'

--- frontend/src/Frontend/KadenaAddress.hs
+++ frontend/src/Frontend/KadenaAddress.hs
@@ -1,83 +1,238 @@
+{-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Frontend.KadenaAddress
-  ( KadenaAddress (unKadenaAddress)
-  , AccountName (unAccountName)
-  , mkAccountName
+  ( KadenaAddressError (..)
+  , KadenaAddress
+    ( _kadenaAddress_encoded
+    , _kadenaAddress_networkName
+    , _kadenaAddress_accountName
+    , _kadenaAddress_chainId
+    , _kadenaAddress_checksum
+    )
+  , mkKadenaAddress
+  , decodeKadenaAddress
+  , bytestringCRC32
+  , mkAddressCRC32
+  , showCRC32
+  , parseKadenaAddressBlob
+  , delimiter
+  , humanReadableDelimiter
+  , textKadenaAddress
+  , isValidKadenaAddress
   ) where
 
-import "cryptonite" Crypto.Hash (Digest, MD5)
+import Control.Lens
+import Control.Monad.Except (MonadError (..), liftEither)
+import Control.Error (note)
+import Control.Monad ((<=<),when)
+
+import qualified Data.Char as C
+import Data.Word (Word8,Word32)
+import Text.Printf (printf)
+import Data.String (fromString)
+import Data.Bifunctor (first)
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString as BS
 
 import Data.Text (Text)
-import GHC.Generics (Generic)
-import Pact.Types.ChainId
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Encoding.Error as TE
 
+import qualified Data.Attoparsec.ByteString.Char8 as A
+
+import qualified URI.ByteString as URI
+import qualified Data.ByteString.Base64 as Base64
+import qualified Data.Digest.CRC32 as Digest
+
+import Pact.Types.ChainId
 import Kadena.SigningApi (AccountName(..), mkAccountName)
 
-import Common.Network (NetworkName)
-import Frontend.Crypto.Ed25519 (PublicKey)
-import Frontend.Foundation (makePactLenses)
-
-newtype KadenaAddress = KadenaAddress { unKadenaAddress :: Text }
-  deriving (Show, Eq)
+import Common.Network (NetworkName,textNetworkName,mkNetworkName)
 
 {-
 Encoder / Decoder should not be derived automatically as package updates could silently
-change the format and break things. Keep to simple handrolled parser & printer that
+change the format and break things. Keep to simple hand-rolled parser & printer that
 satisfies the round-trip property: (print . parse = id), hedgehog has 'tripping' for this.
 
 Final encoding will be Base64 because it needs to be reliably cut'n'paste between
-platforms without risking corruption. (Does this need to be cryptographically secure?)
+platforms without risking corruption.
 
-We probably should tighten the restrictions for some of the components of the address:
+The address will contain the following fields:
 
-Account Name:
-Currently only requires that it not be empty. This should be changed to have a max
-character limit and limitations on the characters that can be entered. Limit to
-alpha-numeric only with '_' and '-' allowed?
+AccountName: Compliant with the requirements contained in pact. In the case of wallet-only
+accounts that do not exist on the blockchain, the Public Key will be used.
+ChainId: Non-negative integer.
+Network: The network where the chain resides.
+Checksum: The checksum will be CRC32 of the 'AccountName/PublicKey', 'ChainId',
+‘Network’. An invalid checksum will be a parse failure.
 
-ChainId:
-At the moment is a numeric identifier. How many chains can we expect to see? It's probably
-acceptable to leave this as is with some extra steps to ensure that this can't be a
-negative value, and can't contain decimal values.
+Some examples of the encoding (delimiter yet to be determined):
 
-NetworkName(Id):
-This is a hostname, which according to the RFC can be up to 253 characters, including '.'
-separators. Do we expect to allow different ports in this configuration? There should be
-library support we can lean on to ensure that the network is RFC compliant and that should
-give us enough reliability.
-
-Public Key & Checksum:
-Both of these are by definition fixed size pieces of information with redefined structures.
-
-Checksum will be MD5 hash of 'AccountName', 'ChainId', 'NetworkName', 'PublicKey'. This
-will be the last field as well so we can parse the initial fields, create the checksum,
-parse the checksum and compare, failing immediately if we don't have a match.
-
-Some ideas for layout:
-
-Newlines:
-
-us1.tn1.chainweb.com
-1
+Using newlines:
 doug
-1bfafb513a26979b48bafe3f9c0fba7703065066a84421f71ca41b54692de67d 
+1
+us1.tn1.chainweb.com
+1bfafb513a26979b48bafe3f9c0fba7703065066a84421f71ca41b54692de67d
 71f920fa275127a7b60fa4d4d41432a3
 
 Or some other delimiter:
+doug%1%us1.tn1.chainweb.com%1bfafb513a26979b48bafe3f9c0fba7703065066a84421f71ca41b54692de67d%71f920fa275127a7b60fa4d4d41432a3
 
-us1.tn1.chainweb.com%1%doug%1bfafb513a26979b48bafe3f9c0fba7703065066a84421f71ca41b54692de67d%71f920fa275127a7b60fa4d4d41432a3
+The final form includes a human readable component as well as machine readable components:
 
+<account name>][<chain id>][<base64 encoding of address>
+
+The use of an address does not require the human readable component. Only the Base64
+encoded address is validated against the checksum. If the human readable prefix is
+included then it will be checked against the values decoded in the address as an extra
+preventative measure.
 -}
 
+data KadenaAddressError
+  = ParseError Text
+  | Crc32Error
+  | Base64Error String
+  | InvalidHumanReadablePiece Text
+  deriving (Show, Eq)
 
--- mkKadenaAddress
---   :: NetworkName
---   -> ChainId
---   -> AccountName
---   -> PublicKey
---   -> KadenaAddress
--- mkKadenaAddress network cid acc pubkey =
---   _F
+data KadenaAddress = KadenaAddress
+  { _kadenaAddress_encoded :: ByteString
+    -- ^ The complete encoded version of this address
+  , _kadenaAddress_accountName :: AccountName
+    -- ^ The account name associated with this address
+  , _kadenaAddress_chainId :: ChainId
+    -- ^ The chain where this account resides
+  , _kadenaAddress_networkName :: NetworkName
+    -- ^ The network where this chain resides
+  , _kadenaAddress_checksum :: Word32
+    -- ^ The CRC32 checksum of the AccountName, ChainId, and NetworkName
+  }
+  deriving (Show, Eq)
 
+textKadenaAddress :: KadenaAddress -> Text
+textKadenaAddress = TE.decodeUtf8With TE.lenientDecode . _kadenaAddress_encoded
+
+humanReadableDelimiter :: Word8
+humanReadableDelimiter = 47 -- forward slash '/'
+
+delimiter :: ByteString
+delimiter = "\n"
+
+bsshow :: Show a => a -> ByteString
+bsshow = fromString . show
+
+-- Show the CRC32 value as a hexadecimal string
+showCRC32 :: Word32 -> String
+showCRC32 = printf "%x"
+
+-- Convert the CRC32 to a bytestring
+bytestringCRC32 :: Word32 -> ByteString
+bytestringCRC32 = fromString . showCRC32
+
+-- Build the checksum for the given information
+mkAddressCRC32 :: AccountName -> ChainId -> NetworkName -> Word32
+mkAddressCRC32 acc cid net = Digest.crc32 $ name <> bsshow cid <> net0
+  where
+    name = TE.encodeUtf8 (unAccountName acc)
+    net0 = TE.encodeUtf8 $ textNetworkName net
+
+isValidKadenaAddress :: KadenaAddress -> Bool
+isValidKadenaAddress ka =
+  let crc = mkAddressCRC32
+        (_kadenaAddress_accountName ka)
+        (_kadenaAddress_chainId ka)
+        (_kadenaAddress_networkName ka)
+
+      decoded = decodeKadenaAddress (_kadenaAddress_encoded ka)
+  in
+    _kadenaAddress_checksum ka == crc && Right ka == decoded
+
+mkKadenaAddress
+  :: NetworkName
+  -> ChainId
+  -> AccountName
+  -> KadenaAddress
+mkKadenaAddress network cid acc =
+  let
+    bcid = TE.encodeUtf8 (cid ^. chainId)
+    name = TE.encodeUtf8 $ unAccountName acc
+    net = TE.encodeUtf8 $ textNetworkName network
+    checksum = mkAddressCRC32 acc cid network
+
+    encoded = Base64.encode $ mconcat
+      [ name
+      , delimiter
+      , bcid
+      , delimiter
+      , net
+      , delimiter
+      , bytestringCRC32 checksum
+      ]
+  in
+    KadenaAddress (name <> cons humanReadableDelimiter bcid <> cons humanReadableDelimiter encoded)
+      acc
+      cid
+      network
+      checksum
+
+decodeKadenaAddress
+  :: ByteString
+  -> Either KadenaAddressError KadenaAddress
+decodeKadenaAddress inp = do
+  let hr = attemptHumanReadable
+      mname = hr ^? _Right . _1
+      mcid = hr ^? _Right . _2
+      pkg0 = either id (^. _3) hr
+
+  blob <- liftEither $ first Base64Error $ Base64.decode pkg0
+  (name, cid, net, crc) <- first (ParseError . T.pack) $ A.parseOnly parseKadenaAddressBlob blob
+
+  -- Final validation steps
+  when (crc /= mkAddressCRC32 name cid net) $ throwError Crc32Error
+
+  let checkpiece og nm mbs
+        | Just bs <- mbs, bs /= TE.encodeUtf8 og = throwError (InvalidHumanReadablePiece nm)
+        | otherwise = pure ()
+
+  -- If we have the human readable components, do an extra check that everything matches
+  _ <- checkpiece (unAccountName name) "AccountName" mname
+  _ <- checkpiece (cid ^. chainId) "Chain Id" mcid
+
+  pure (KadenaAddress inp name cid net crc)
+  where
+    attemptHumanReadable = case BS.split humanReadableDelimiter inp of
+      [name, cid, pkg] -> Right (name, cid, pkg)
+      _ -> Left inp
+
+failParse :: Either String a -> A.Parser a
+failParse = either fail pure
+
+parseKadenaAddressBlob :: A.Parser (AccountName, ChainId, NetworkName, Word32)
+parseKadenaAddressBlob = (,,,)
+  <$> parseAccountName
+  <*> parseChainId
+  <*> parseNetworkName
+  <*> A.hexadecimal
+  where
+    mkParser p c = (c <$> p) >>= failParse
+    latin1ToEOL = A.manyTill (A.satisfy C.isLatin1) A.endOfLine
+
+    parseAccountName = mkParser latin1ToEOL (first T.unpack . mkAccountName . T.pack)
+    parseNetworkName = mkParser latin1ToEOL (networkNameParser . BS8.pack)
+    networkNameParser = first T.unpack . mkNetworkName <=< parseURIToText
+
+    parseURIToText bs = do
+      uri <- first show $ URI.parseRelativeRef URI.strictURIParserOptions bs
+      note "Missing Network Name" (getHost uri)
+
+    parseChainId = mkParser
+      (A.manyTill A.digit A.endOfLine)
+      (note "Invalid ChainId" . pure . ChainId . T.pack)
+
+getHost :: URI.URIRef a -> Maybe Text
+getHost = preview (URI.pathL . to (TE.decodeUtf8With TE.lenientDecode))
