8cde86dbed8eaba08ef787e28ad2f8a8c2cb6d53
Tom Smalley <tfsmalley@gmail.com>
2019-10-31 19:11:55 +0000

    Add missing pragma

--- desktop/src/Desktop/Setup.hs
+++ desktop/src/Desktop/Setup.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
