a3165461f51f279b727171a8b0bc657f848c7b7d
Ben Kolera <ben.kolera@gmail.com>
2019-11-20 03:47:54 +1000

    fix: Shrinks the height on the wallet buttons. Horizontally pads out hamburger button.

--- frontend/src/Frontend/UI/Wallet.hs
+++ frontend/src/Frontend/UI/Wallet.hs
@@ -190,7 +190,7 @@ uiKeyItem selectedNetwork i d = do
 
               recv <- receiveButton cfg
               void $ sendButton $ cfg & uiButtonCfg_disabled .~ True
-              onDetails <- detailsButton cfg
+              onDetails <- detailsButton (cfg & uiButtonCfg_class <>~ " wallet__table-button--hamburger")
 
               let mkDialog dia onE = (\a -> (dia, i, a)) <$> current account <@ onE
 
