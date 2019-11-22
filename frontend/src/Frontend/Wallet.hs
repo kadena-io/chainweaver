d777eb42013d04016f9c13c2c00b7e86bffb64a5
Tom Smalley <tfsmalley@gmail.com>
2019-11-22 15:00:34 +0000

    Elimination

--- frontend/src/Frontend/Wallet.hs
+++ frontend/src/Frontend/Wallet.hs
@@ -84,9 +84,7 @@ import Frontend.Network
 accountToKadenaAddress :: Account key -> KadenaAddress
 accountToKadenaAddress a = mkKadenaAddress isCreated (_account_chainId a) (_account_name a)
   where
-    isCreated = case _account_balance a of
-      Just _ -> AccountCreated_Yes
-      Nothing -> AccountCreated_No
+    isCreated = maybe AccountCreated_No (const AccountCreated_Yes) $ _account_balance a
 
 data WalletCfg key t = WalletCfg
   { _walletCfg_genKey     :: Event t (AccountName, NetworkName, ChainId, Text)
