9591597dc2c5d0a223b66ddc55bf9942b148355b
Doug Beardsley <mightybyte@gmail.com>
2019-11-29 17:37:34 -0500

    Enable mainnet

--- common/src/Common/Network.hs
+++ common/src/Common/Network.hs
@@ -69,9 +69,7 @@ instance ToJSONKey NetworkName where
 
 -- | Construct a 'NetworkName', and banish mainnet - for now.
 mkNetworkName :: Text -> Either Text NetworkName
-mkNetworkName (T.strip -> t)
-  | "mainnet" `T.isInfixOf` t = Left "This wallet does not support mainnet"
-  | otherwise = Right $ NetworkName t
+mkNetworkName (T.strip -> t) = Right $ NetworkName t
 
 -- | Construct a 'NetworkName' but don't perform any checks
 uncheckedNetworkName :: Text -> NetworkName
