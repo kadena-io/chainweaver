374bf94dcd327481ac19ff0ef6a92352ea9bb75e
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-12-19 23:37:12 +0000

    Also build 'desktop' derivation in both platforms

--- release.nix
+++ release.nix
@@ -3,7 +3,10 @@ let self = import ./. {};
 
     mac   = { inherit (self) mac; };
     linux = { };
-    cross = { inherit (self) exe; };
+    cross = {
+      inherit (self) exe;
+      inherit (self.ghc) desktop;
+    };
 in
   cross
   // (if system == "x86_64-darwin" then mac else {})
