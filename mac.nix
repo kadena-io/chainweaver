752b97eca1e5cebb665791ec8ebccf14ccb5f749
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-01-22 21:49:43 +0000

    Always parse certificate with LibreSSL

--- mac.nix
+++ mac.nix
@@ -149,7 +149,7 @@ in rec {
       | sed 's|    "alis"<blob>="\(.*\)"$|\1|' \
       | while read c; do \
           security find-certificate -c "$c" -p \
-            | openssl x509 -subject -noout; \
+            | ${pkgs.libressl}/bin/openssl x509 -subject -noout; \
         done \
       | grep "OU[[:space:]]\?=[[:space:]]\?$TEAM_ID" \
       | sed 's|subject= /UID=[^/]*/CN=\([^/]*\).*|\1|' \
