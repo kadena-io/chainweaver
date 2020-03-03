a852442f55c1eb0c4ad2a912746ad6ba1122a621
Sean Chalmers <sean.chalmers@obsidian.systems>
2020-03-03 16:00:33 +0000

    Bumped version to 1.2.0

--- default.nix
+++ default.nix
@@ -8,11 +8,11 @@ with obelisk;
 let
   # All the versions that the user cares about are here so that they can
   # be changed in one place
-  chainweaverVersion = "1.1";
+  chainweaverVersion = "1.2";
   appName = "Kadena Chainweaver";
-  macReleaseNumber = "2";
-  linuxReleaseNumber = "2";
-  ovaReleaseNumber = "2";
+  macReleaseNumber = "0";
+  linuxReleaseNumber = "0";
+  ovaReleaseNumber = "0";
 
   obApp = import ./obApp.nix args;
   pactServerModule = import ./pact-server/service.nix;
