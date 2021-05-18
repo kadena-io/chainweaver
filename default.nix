8d6f003a083711c9ffd54761e57478af9c8614e7
Doug Beardsley <mightybyte@gmail.com>
2021-05-18 09:35:02 -0400

    Fix version

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ with obelisk;
 let
   # All the versions that the user cares about are here so that they can
   # be changed in one place
-  chainweaverVersion = "2.2";
+  chainweaverVersion = "2.1";
   appName = "Kadena Chainweaver";
   macReleaseNumber = "1";
   linuxReleaseNumber = "1";
