73968a63068a2cf5372a0d0084b56a9c8c102966
jmininger <no email>
2021-07-29 14:17:32 -0400

    add back removed code as commented out so  that we can come back to it

--- desktop/src/Desktop/WalletApi.hs
+++ desktop/src/Desktop/WalletApi.hs
@@ -29,13 +29,25 @@ walletServer
   => IO ()
   -> IO ()
   -> m (MVarHandler SigningRequest SigningResponse)
+  -- Commented much of the old functionality out (vs. removing it) since we intend to revisit it in the near future
+  -- -> m ( MVarHandler SigningRequest SigningResponse
+  --      , MVarHandler () [PublicKey]
+  --      , MVarHandler () (Map NetworkName [AccountName])
+  --      )
 walletServer moveToForeground moveToBackground = do
   signingLock <- liftIO newEmptyMVar -- Only allow one signing request to be served at once
   h@(MVarHandler signingRequestMVar signingResponseMVar) <- liftIO newMVarHandler
+  -- keysHandler <- liftIO newMVarHandler
+  -- accountsHandler <- liftIO newMVarHandler
   let
     runSign obj = mkServantHandler <=< liftIO $ bracket_ (putMVar signingLock ()) (takeMVar signingLock) $ do
         putMVar signingRequestMVar obj -- handoff to app
         bracket moveToForeground (const $ moveToBackground) (\_ -> takeMVar signingResponseMVar)
+
+    -- runMVarHandler (MVarHandler req resp) = do
+    --   mkServantHandler <=< liftIO $ do
+    --     putMVar req ()
+    --     takeMVar resp
 
     mkServantHandler = \case
       Left e -> throwError $ Servant.err409 { Servant.errBody = LBS.fromStrict $ T.encodeUtf8 e }
@@ -46,13 +58,16 @@ walletServer moveToForeground moveToBackground = do
       { Wai.corsRequestHeaders = Wai.simpleHeaders }
     apiServer
       = Warp.runSettings s $ Wai.cors laxCors
-      $ Servant.serve walletApi runSign
+      $ Servant.serve walletApi $ runSign   --   :<|> runMVarHandler keysHandler :<|> runMVarHandler accountsHandler
 
   liftIO $ void $ Async.async $ apiServer
+  -- pure (h, keysHandler, accountsHandler)
   pure h
 
 type WalletAPI = "v1" :> V1WalletAPI
 type V1WalletAPI = V1SigningApi
+                   -- :<|> "keys" :> Get '[JSON] [PublicKey]
+                   -- :<|> "accounts" :> Get '[JSON] (Map NetworkName [AccountName])
 
 walletApi :: Proxy WalletAPI
 walletApi = Proxy
