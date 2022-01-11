b29caae1d3dad7b07366ddfc52bdcaebb233d0c6
jmininger <no email>
2022-01-11 17:11:24 -0500

    support partial and non-cw sigs

--- frontend/src/Frontend/UI/Dialogs/SigBuilder.hs
+++ frontend/src/Frontend/UI/Dialogs/SigBuilder.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -7,20 +8,22 @@
 
 module Frontend.UI.Dialogs.SigBuilder where
 
-import           Control.Error
+import           Control.Error hiding (bool, mapMaybe)
 import           Control.Lens
-import           Control.Monad (join)
+import           Control.Monad (forM, join)
 import qualified Data.Aeson as A
 import qualified Data.Set as Set
 import           Data.Aeson.Parser.Internal (jsonEOF')
 import           Data.Attoparsec.ByteString
-import           Data.Bifunctor (first)
+import           Data.Bifunctor (first, second)
 import qualified Data.ByteString.Lazy as LB
 import           Data.Functor (void)
+import           Data.List (partition)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
+import qualified Data.Map as Map
 import qualified Data.IntMap as IMap
 import           Data.YAML
 import qualified Data.YAML.Aeson as Y
@@ -28,19 +31,22 @@ import           Pact.Types.ChainMeta
 import           Pact.Types.ChainId   (networkId)
 import           Pact.Types.RPC
 import           Pact.Types.Command
-import           Pact.Types.Hash (hash, toUntypedHash, unHash)
+import           Pact.Types.Hash (Hash, hash, toUntypedHash, unHash)
 import           Pact.Types.SigData
 import           Pact.Types.Util             (decodeBase64UrlUnpadded)
 import           Pact.Types.Pretty
 import           Pact.Types.Info
+import qualified Pact.Types.Term                   as Pact
 ------------------------------------------------------------------------------
 import           Reflex
-import           Reflex.Dom hiding (Key)
+import           Reflex.Dom hiding (Key, Command)
 ------------------------------------------------------------------------------
 import           Common.Wallet
 import           Frontend.Crypto.Class
 import           Frontend.Foundation
+import           Frontend.Log
 import           Frontend.Network
+import           Frontend.UI.Dialogs.DeployConfirmation
 import           Frontend.UI.Modal
 import           Frontend.UI.Transfer
 import           Frontend.UI.Widgets
@@ -117,7 +123,7 @@ warningDialog warningMsg backW nextW = Workflow $ do
     el "p" $ text warningMsg
   (back, next) <- modalFooter $ (,)
     <$> confirmButton def "Back"
-    <*> confirmButton def "Next"
+    <*> confirmButton def "Continue"
   pure $ (onClose, leftmost [ backW <$ back , nextW <$ next ])
 
 errorDialog
@@ -139,8 +145,7 @@ checkAndSummarize
   -> SigBuilderRequest key
   -> Workflow t m (Event t ())
 checkAndSummarize model sbr =
-  -- let
-  --   sigsRequired = 
+  -- TODO: Add "hash-mismatch" error when Sig-Data hash and payload hash are not identical
   if Set.disjoint cwPubKeys signerPubkeys
      then errorDialog "Chainweaver does not possess any of the keys required for signing" backW
      else checkNetwork
@@ -177,21 +182,31 @@ approveSigDialog
 approveSigDialog model sbr = Workflow $ do
   let sigData = _sbr_sigData sbr
       keys = fmap _key_pair $ _sbr_cwKeys sbr
+      dNodeInfos = model^.network_selectedNodes
   onClose <- modalHeader $ text "Approve Transaction"
-  modalMain $ do
+  sigsOrKeys <- modalMain $ do
     let p = _sbr_payload sbr
+        sdHash = toUntypedHash $ _sigDataHash $ _sbr_sigData sbr
     networkWidget p
     txMetaWidget  $ _pMeta p
-    pactRpcWidget  $ _pPayload p 
+    sigsOrKeys' <- signerWidget (_keyPair_publicKey <$> keys) (_pSigners p) sdHash
+    pactRpcWidget  $ _pPayload p
+    pure $ sequence sigsOrKeys'
 
-  (back, sign, signAndSubmit) <- modalFooter $ (,,)
+  (back, sign) <- modalFooter $ (,)
     <$> confirmButton def "Back"
     <*> confirmButton def "Sign"
-    <*> confirmButton def "Sign And Send"
+  let sigsOnSubmit = mapMaybe (\(a, mb) -> fmap (a,) mb) <$> current sigsOrKeys <@ sign
   let workflowEvent = leftmost
         [ txnInputDialog model (Just sigData) <$ back
-        , signAndShowSigDialog model sbr keys <$ sign
+        , signAndShowSigDialog model sbr keys <$> sigsOnSubmit
+        -- , transferAndStatus model nodeInfos (sender, cid) cmd <$ signAndSubmit
+        -- , transferAndStatus model (AccountName "jacquin", ChainId "0") cmd nodeInfos <$ signAndSubmit
         ]
+
+  -- => Hash
+  -- -> PublicKey
+  -- -> m (Dynamic t (PublicKeyHex, Maybe UserSig))
   return (onClose, workflowEvent)
 
 signAndShowSigDialog
@@ -199,23 +214,54 @@ signAndShowSigDialog
   => ModalIde m key t
   -> SigBuilderRequest key
   -> [KeyPair key]
+  -> [(PublicKeyHex, UserSig)]
   -> Workflow t m (Event t ())
-signAndShowSigDialog _model sbr keys = Workflow $ do
-
-  sigOrErr <- buildSigDataWithPayload (_sbr_payload sbr) keys
+signAndShowSigDialog _model sbr keys sigsOrPrivate = Workflow $ do
   onClose <- modalHeader $ text "Signed Txn"
-  modalMain $ do
-    case sigOrErr of
-      Left e-> text $ "Error: Signing failed with: " <> (T.pack e)
-      Right sd -> do
-        let txt = T.decodeUtf8 $ Y.encode1Strict sd
-        void $ uiTxSigner (Just sd) def def
-        uiDetailsCopyButton $ constant txt 
-    
+  -- This allows a "loading" page to render before we attempt to do the really computationally
+  -- expensive sigs
+  pb <- delay 0.0 =<< getPostBuild
+  -- TODO: Can we forkIO the sig process and display something after they are done?
+  widgetHold_ (modalMain $ text "Loading Signatures ...") $ ffor pb $ \_ ->
+    modalMain $ do
+      let sd = _sbr_sigData sbr
+      sd' <- addSigsToSigData sd keys sigsOrPrivate
+      let txt = T.decodeUtf8 $ Y.encode1Strict sd
+      void $ uiTxSigner (Just sd) def def
+      uiDetailsCopyButton $ constant txt
+
   (back, finish) <- modalFooter $ (,)
     <$> confirmButton def "Back"
     <*> confirmButton def "Finish"
   return (onClose <> finish, never)
+
+addSigsToSigData
+  :: (
+       MonadJSM m
+     , HasCrypto key m
+     )
+  => SigData Text
+  -> [KeyPair key]
+  -- ^ Keys which we are signing with
+  -> [(PublicKeyHex, UserSig)]
+  -> m (SigData Text)
+addSigsToSigData sd signingKeys sigList = do
+  --TODO: Double check hash here and fail if they dontmatch
+  let -- cmd = encodeAsText $ encode payload
+      -- cmdHashL = hash (T.encodeUtf8 cmd)
+      hash = unHash $ toUntypedHash $ _sigDataHash sd
+      someSigs = _sigDataSigs sd
+      pubKeyToTxt =  PublicKeyHex . keyToText . _keyPair_publicKey 
+      cwSigners =  fmap (\x -> (pubKeyToTxt x, _keyPair_privateKey x)) signingKeys
+      toPactSig sig = UserSig $ keyToText sig
+  sigList <- forM someSigs $ \(pkHex, mSig) -> case mSig of
+    Just sig -> pure (pkHex, Just sig)
+    Nothing -> case pkHex `lookup` cwSigners of
+      Just (Just priv) -> do
+        sig <- cryptoSign hash priv
+        pure (pkHex, Just $ toPactSig sig )
+      otherwise -> pure (pkHex, pkHex `lookup` sigList)
+  pure $ sd { _sigDataSigs = sigList }
 
 data SigBuilderRequest key = SigBuilderRequest
   { _sbr_sigData        :: SigData Text
@@ -223,6 +269,61 @@ data SigBuilderRequest key = SigBuilderRequest
   , _sbr_currentNetwork :: Maybe NetworkName
   , _sbr_cwKeys         :: [ Key key ]
   }
+
+signerWidget
+  :: (MonadWidget t m, HasCrypto key m)
+  => [PublicKey]
+  -> [Signer]
+  -> Hash
+  -> m [Dynamic t (PublicKeyHex, Maybe UserSig)]
+signerWidget cwKeys signers sdHash = do
+  let (unscoped, scoped) = partition isUnscoped signers
+  dUnscoped <- ifEmptyBlankSigner unscoped $ do
+    dialogSectionHeading mempty "Unscoped Signers"
+    divClass "group" $ do
+      fmap catMaybes $ mapM unscopedSignerRow unscoped
+
+  dScoped <- ifEmptyBlankSigner scoped $ do
+    dialogSectionHeading mempty "Scoped Signers"
+    fmap catMaybes $ mapM scopedSignerRow scoped
+  let sigsOrKeys = dScoped <> dUnscoped
+  pure sigsOrKeys
+
+  where
+    isUnscoped (Signer _ _ _ capList) = capList == []
+    ifEmptyBlankSigner l w = if l == [] then (blank >> pure []) else w
+    unOwnedSigningInput pub =
+      if pub `elem` cwKeys
+        then blank >> pure Nothing
+        else fmap Just $ uiSigningInput sdHash pub
+
+    unscopedSignerRow s = do
+      el "div" $ do
+        maybe blank (text . (<> ":") . tshow) $ _siScheme s
+        text $ _siPubKey s
+        maybe blank (text . (":" <>) . tshow) $ _siAddress s
+      elAttr "ul" ("style" =: "margin-block-start: 0; margin-block-end: 0;") $
+        mapM_ (elAttr "li" ("style" =: "list-style-type: none;") . text . renderCompactText) $ _siCapList s
+      -- uiSigningInput sdHash pkey
+      let (Right pkey) = parsePublicKey $ _siPubKey s
+      unOwnedSigningInput pkey
+
+    scopedSignerRow signer = do
+      divClass "group" $ do
+        visible <- divClass "signer__row" $ do
+          let accordionCell o = (if o then "" else "accordion-collapsed ") <> "payload__accordion "
+          rec
+            clk <- elDynClass "div" (accordionCell <$> visible') $ accordionButton def
+            visible' <- toggle True clk
+          divClass "signer__pubkey" $ text $ _siPubKey signer
+          pure visible'
+        elDynAttr "div" (ffor visible $ bool ("hidden"=:mempty) mempty)$ do
+          el "ul" $ forM_ (_siCapList signer) $ \cap ->
+            elAttr "li" ("style" =: "list-style-type:none;")$ text $ renderCompactText cap
+        let (Right pkey) = parsePublicKey $ _siPubKey signer
+        unOwnedSigningInput pkey
+
+
 
 txMetaWidget
   :: MonadWidget t m
@@ -321,3 +422,17 @@ parseInputToSigDataWidget mInit =
     validInput DTB_EmptyString = Nothing
     validInput (DTB_ErrorString _) = Nothing
     validInput (DTB_SigData info) = Just info
+
+transferAndStatus
+  :: (MonadWidget t m, Monoid mConf, HasLogger model t, HasTransactionLogger m)
+  => model
+  -> (AccountName, ChainId)
+  -> Command Text
+  -> [NodeInfo]
+  -> Workflow t m (Event t ())
+transferAndStatus model (sender, cid) cmd nodeInfos = Workflow $ do
+    close <- modalHeader $ text "Transfer Status"
+    _ <- elClass "div" "modal__main transaction_details" $
+      submitTransactionWithFeedback model cmd sender cid (fmap Right nodeInfos)
+    done <- modalFooter $ confirmButton def "Done"
+    pure (close <> done, never)
