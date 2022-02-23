0a6a55b19c2498f98b0a0f4d507afb417159960c
Anurag Ohri <anuragohri92@gmail.com>
2022-02-23 13:12:47 +0530

    Added all global variables to externs.js

--- static/externs.js
+++ static/externs.js
@@ -1,21 +1,6 @@
-// var ace = false;
-var GlobalTester = (function(){
-  var fields = {};
-  var before = function(w){
-      for(var field in w){
-          fields[field] = true;
-      };
-  };
-
-  var after = function(w){
-      for(var field in w){
-          if(!fields[field]){
-               console.log(field + " has been added");
-          }
-      };
-
-  };
-  return {before: before, after:after};
-}());
-
-GlobalTester.before(window);
+var define = false;
+var require = false;
+var ace = false;
+var nacl = false;
+var lib = false;
+var bowser = false;
--- static/test.js
+++
@@ -1,1 +0,0 @@
-GlobalTester.after(window);
