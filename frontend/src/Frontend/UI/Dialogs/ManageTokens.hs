0ee199c2414b45e38249cca0c5341b8fdf938ee7
Anurag Ohri <anuragohri92@gmail.com>
2022-03-31 18:14:21 +0530

    Fixed bug where Manage Tokens dialog was loading only once

--- frontend/src/Frontend/UI/Dialogs/ManageTokens.hs
+++ frontend/src/Frontend/UI/Dialogs/ManageTokens.hs
@@ -11,6 +11,7 @@ import Control.Monad (forM, void)
 import Data.Bifunctor (first)
 import Data.Either (rights)
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Map as Map
 import qualified Data.Text as T
 import Reflex.Dom
 
@@ -37,10 +38,9 @@ uiManageTokens
      )
   => model -> Event t () -> m (mConf, Event t ())
 uiManageTokens model onCloseExternal = do
-  (conf, closes) <- fmap splitDynPure $ workflow $
-    inputToken model onCloseExternal
-  mConf <- flatten =<< tagOnPostBuild conf
-  let close = switch $ current closes
+  (conf, closes) <- splitE <$> inputToken model onCloseExternal
+  mConf <- flatten conf
+  close <- switchHold never closes
   pure (mConf, close)
 
 -- | Allow the user to input a new fungible
@@ -53,52 +53,49 @@ inputToken
      )
   => model
   -> Event t () -- ^ Modal was externally closed
-  -> Workflow t m (mConf, Event t ())
-inputToken model _ =
-  let
-    next = Workflow $ do
-      close <- modalHeader $ text "Manage Tokens"
-      (dmFung, clickEv, addEv) <- modalMain $ do
-        tokenList <- sample $ current $ model ^. wallet_tokenList
-        currentFung <- sample $ current $ model ^. wallet_fungible
+  -> m (Event t (mConf, Event t ()))
+inputToken model _ = do
+  close <- modalHeader $ text "Manage Tokens"
+  networkView $ model ^. network_modules <&> \moduleMap ->
+    if Map.null moduleMap
+      then do
+        void $ modalMain $ text "Loading..."
+        pure mempty
+      else do
+        (dmFung, clickEv, addEv) <- modalMain $ do
+          tokenList <- sample $ current $ model ^. wallet_tokenList
+          currentFung <- sample $ current $ model ^. wallet_fungible
 
-        let
-          tokenValidator textEv = do
-            let
-              isValidToken moduleList token = case parseModuleName token of
-                Left err -> Failure $ "Invalid Token: " <> T.pack err
-                Right mdule -> if mdule `elem` moduleList
-                  then Success mdule
-                  else Failure "Unsupported Token"
-            pure $ isValidToken <$> current (moduleListDyn model) <@> textEv
+          let
+            tokenValidator textEv = do
+              let
+                isValidToken moduleList token = case parseModuleName token of
+                  Left err -> Failure $ "Invalid Token: " <> T.pack err
+                  Right mdule -> if mdule `elem` moduleList
+                    then Success mdule
+                    else Failure "Unsupported Token"
+              pure $ isValidToken <$> current (moduleListDyn model) <@> textEv
 
-        dialogSectionHeading mempty "Tokens"
-        (dmFung, addEv) <- divClass "flex" $ do
-          dmFung <- divClass "group flex-grow" $
-            fmap snd $ uiTextInputAsync "Enter token" True (Nothing, PopoverState_Disabled) never tokenValidator moduleListId
-          addEv <- confirmButton (def & uiButtonCfg_class .~ "margin" & uiButtonCfg_disabled .~ (isNothing <$> dmFung)) "Add"
-          pure (dmFung, addEv)
-        eventEv <- networkView $ model ^. wallet_tokenList <&> \ne -> do
-          clicks <- forM (NE.toList ne) $ \token -> do
-            (e, _) <- accordionItem' False "segment segment_type_secondary"
-              (do
-                text $ _mnName token
-                deleteButtonNaked $ def & uiButtonCfg_class .~ "accordion__title-button")
-              (text $ renderCompactText token)
-            pure $ token <$ e
-          pure $ leftmost clicks
-        clickEv <- switchHold never eventEv
-        pure (dmFung, clickEv, addEv)
-      done <- modalFooter $ do
-        confirmButton def "Done"
-      let 
-        fungE = tagMaybe (current dmFung) done
-        conf = mempty & walletCfg_fungibleModule .~ fungE & walletCfg_delModule .~ clickEv & walletCfg_addModule .~ (fmapMaybe id $ tag (current dmFung) addEv)
-      pure ( (conf, done <> close)
-          , never
-          )
-  in
-    Workflow $ do
-      close <- modalHeader $ text "Manage Tokens"
-      void $ modalMain $ text "Loading..."
-      pure ((mempty, close), next <$ updated (model ^. network_modules))
+          dialogSectionHeading mempty "Tokens"
+          (dmFung, addEv) <- divClass "flex" $ do
+            dmFung <- divClass "group flex-grow" $
+              fmap snd $ uiTextInputAsync "Enter token" True (Nothing, PopoverState_Disabled) never tokenValidator moduleListId
+            addEv <- confirmButton (def & uiButtonCfg_class .~ "margin" & uiButtonCfg_disabled .~ (isNothing <$> dmFung)) "Add"
+            pure (dmFung, addEv)
+          eventEv <- networkView $ model ^. wallet_tokenList <&> \ne -> do
+            clicks <- forM (NE.toList ne) $ \token -> do
+              (e, _) <- accordionItem' False "segment segment_type_secondary"
+                (do
+                  text $ _mnName token
+                  deleteButtonNaked $ def & uiButtonCfg_class .~ "accordion__title-button")
+                (text $ renderCompactText token)
+              pure $ token <$ e
+            pure $ leftmost clicks
+          clickEv <- switchHold never eventEv
+          pure (dmFung, clickEv, addEv)
+        done <- modalFooter $ do
+          confirmButton def "Done"
+        let 
+          fungE = tagMaybe (current dmFung) done
+          conf = mempty & walletCfg_fungibleModule .~ fungE & walletCfg_delModule .~ clickEv & walletCfg_addModule .~ (fmapMaybe id $ tag (current dmFung) addEv)
+        pure (conf, done <> close)
