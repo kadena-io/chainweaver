422294fcae69964c67097ca4e76e12444fd48119
jmininger <no email>
2022-04-21 19:11:24 -0400

    WIP: progress in validator func

--- frontend/src/Frontend/UI/Dialogs/ManageTokens.hs
+++ frontend/src/Frontend/UI/Dialogs/ManageTokens.hs
@@ -22,6 +22,7 @@ import Pact.Types.Exp
 import Pact.Types.PactValue
 import Pact.Types.Pretty  (renderCompactText)
 import Pact.Types.Names   (parseModuleName)
+import Pact.Types.Runtime (ModuleName)
 
 import Frontend.Crypto.Class (HasCrypto(..))
 import Frontend.Foundation hiding (Arg)
@@ -38,7 +39,7 @@ import Frontend.Wallet
 --
 -- [(1, ['a', 'b']), (2, ['b', 'c'])] => [('a', [1]), ('b', [1,2]), ('c', [2])]
 invertMap :: (Ord a) => Map.Map k [a] -> Map.Map a [k]
-invertMap m = foldl' (\mp (k, as) -> 
+invertMap m = foldl' (\mp (k, as) ->
   foldl' (\mp1 a -> Map.insertWith (++) a [k] mp1) mp as
   ) Map.empty $ Map.toList m
 
@@ -61,6 +62,92 @@ uiManageTokens model onCloseExternal = do
   close <- switchHold never closes
   pure (mConf, close)
 
+tokenInput
+  :: ( DomBuilder t m
+     , PostBuild t m
+     , MonadHold t m
+     , DomBuilderSpace m ~ GhcjsDomSpace
+     , PerformEvent t m
+     , TriggerEvent t m
+     , MonadJSM (Performable m)
+     , MonadFix m
+     , HasLogger model t
+     -- , HasWallet model key t
+     )
+  => Dynamic t (Network t)
+  -> Map.Map ChainId ModuleName
+  -> Behavior t [ModuleName]
+  -> m (Dynamic t (Maybe ModuleName))
+tokenInput model net chainToModuleMap bLocalList= do
+  netNAM <- sample $ current $ getNetworkNameAndMeta net
+  let tokenValidator' = tokenValidator net netNAM bLocalList
+  (ti, _) <- mkLabeledInput True "Enter Token" (textFormWidgetAsync PopoverState_Disabled tokenValidator') def
+  -- => PopoverState
+  -- -> (Event t Text -> m (Event t (ValidationResult Text a)))
+  -- -> PrimFormWidgetConfig t (Maybe Text)
+  -- -> m (FormWidget t (Maybe a), Event t (Maybe Text))
+  -- textFormWidgetAsync initPopState isValid cfg = mdo
+  pure $ constDyn Nothing
+  where
+    moduleToChainMap = invertMap chainToModuleMap
+
+    -- TODO: Is this an appropriate way to query all chains a node provides access to?
+    chainList = Map.keys chainToModuleMap
+
+    isModuleFungiblePact moduleName =
+      -- Writing the query in this way allows us to restrict the functions that can produce an error
+      -- In the query below, only the `describe-module` function can generate an error.
+      -- If `describe-module` generated an error, it means that the user entered module is not present on any chain.
+      -- If `describe-module` didn't generate any error, it means that the user entered module is a valid module,
+      --      though it might not be a valid "fungible-v2" contract. For this contract, we should return a False.
+      "(let ((moduleDesc (describe-module \"" <> moduleName <> "\")))" <>
+        "(contains \"fungible-v2\"" <>
+          "(if (contains 'interfaces moduleDesc)" <>
+            "(at 'interfaces moduleDesc)" <>
+            "[]" <>
+            ")))"
+
+    checkModuleIsFungible (netName, netMetadata) evModule = do
+      let
+        -- In case the module exists on some chains, this will store the list of those chainIds.
+        -- If the module does not exist on any chains, it will store all the chainIds.
+        chainIdsToCheck = fromMaybe chainList . flip Map.lookup moduleToChainMap
+
+        reqEv = evModule <&> \mdule -> forM (chainIdsToCheck mdule) $ \chainId ->
+                    mkSimpleReadReq (createCode (renderCompactText mdule)) netName netMetadata $ ChainRef Nothing chainId
+      respEv <- performLocalRead (model ^. logger) net reqEv
+      pure $ respEv <&> \responses ->
+        -- In the following foldM, we have used Either as a Monad to short circuit the fold.
+        -- We short circuit the fold as soon as we find a `True`.
+        -- Note: In order to short circuit, we need to use `Left`, which is commonly used for errors.
+        --       Here, `Left` does NOT represent errors, only a way to short circuit.
+        -- If we encounter an unknown error, we keep the already existing error with us.
+        either id id $
+          foldM (\err (_, netErrorResult) ->
+            case netErrorResult of
+              That (_, pVal) -> case pVal of
+                PLiteral (LBool b) -> if b
+                  then Left $ Success mdule
+                  else Right $ Failure "Contract not a token"
+                x -> Right err
+              _ -> Right err
+            ) (Failure "This module does not exist on any chain") responses
+
+    tokenValidator net netAndMeta bLocalList inputEv = do
+      let
+        localList = bLocalList <@ inputEv
+        -- eNetAndMeta = tag (current $ getNetworkNameAndMeta net) inputEv
+        (failureEv, validNameEv) = fanEither $ ffor inputEv $ \rawInput -> do
+          case parseModuleName rawInput of
+            -- User input is not a valid module name
+            Left err -> Left $ Failure "Invalid module name"
+            -- User input is a valid module name, check if it already exists on any chain
+            Right mdule -> case mdule `elem` localList of
+              True -> Left $ Failure "Token already added"
+              False -> Right mdule
+      resultEv <- switchHold never $ checkModuleIsFungible netAndMeta validNameEv
+      pure $ leftmost [failureEv, resultEv]
+
 -- | Allow the user to input a new fungible
 inputToken
   :: ( Monoid mConf
@@ -78,75 +165,15 @@ inputToken
 inputToken model _ = do
   close <- modalHeader $ text "Manage Tokens"
   networkView $ model ^. network_modules <&> \chainToModuleMap ->
-    if Map.null chainToModuleMap
-      then do
-        void $ modalMain $ text "Loading Tokens..."
-        pure mempty
-      else mdo
+    bool (Map.null chainToModuleMap) (modalMain $ text "Loading Tokens..." >> pure mempty) $
+      mdo
+        network <- sample $ current $ model ^. network
         (dmFung, deleteEv) <- modalMain $ do
           dialogSectionHeading mempty "Tokens"
           dmFung <- divClass "flex" $ mdo
-            let
-              tokenValidator inputEv = do
-                let
-                  moduleToChainMap = invertMap chainToModuleMap
-                  chainList = Map.keys chainToModuleMap
-                failureOrEventDyn <- networkHold (pure $ Right never) $ (,) <$> current localListDyn <@> inputEv <&> \(localList, input) ->
-                  case parseModuleName input of
-                    -- User input is not a valid module name
-                    Left err -> pure $ Left $ Failure "Invalid module name"
-                    -- User input is a valid module name, check if it already exists on any chain
-                    Right mdule -> do
-                      let
-                        -- In case the module exists on some chains, this will store the list of those chainIds.
-                        -- If the module does not exist on any chains, it will store all the chainIds.
-                        chainIdsToCheck = fromMaybe chainList $ Map.lookup mdule moduleToChainMap
-                        net = model ^. network
-                        createCode moduleName =
-                          -- Writing the query in this way allows us to restrict the functions that can produce an error
-                          -- In the query below, only the `describe-module` function can generate an error.
-                          -- If `describe-module` generated an error, it means that the user entered module is not present on any chain.
-                          -- If `describe-module` didn't generate any error, it means that the user entered module is a valid module,
-                          --      though it might not be a valid "fungible-v2" contract. For this contract, we should return a False.
-                          "(let ((moduleDesc (describe-module \"" <> moduleName <> "\")))" <>
-                            "(contains \"fungible-v2\"" <>
-                              "(if (contains 'interfaces moduleDesc)" <>
-                                "(at 'interfaces moduleDesc)" <>
-                                "[]" <>
-                                ")))"
-
-                      if mdule `elem` localList
-                        then pure $ Left $ Failure "Token already added"
-                        else do
-                          reqEv <- networkView $ getNetworkNameAndMeta net <&> \(netName, netMetadata) ->
-                            forM chainIdsToCheck $ \chainId ->
-                              mkSimpleReadReq (createCode (renderCompactText mdule)) netName netMetadata $ ChainRef Nothing chainId
-                          respEv <- performLocalRead (model ^. logger) net reqEv
-                          pure $ Right $ respEv <&> \responses ->
-                            -- In the following foldM, we have used Either as a Monad to short circuit the fold.
-                            -- We short circuit the fold as soon as we find a `True`.
-                            -- Note: In order to short circuit, we need to use `Left`, which is commonly used for errors.
-                            --       Here, `Left` does NOT represent errors, only a way to short circuit.
-                            -- If we encounter an unknown error, we keep the already existing error with us.
-                            either id id $
-                              foldM (\err (_, netErrorResult) ->
-                                case netErrorResult of
-                                  That (_, pVal) -> case pVal of
-                                    PLiteral (LBool b) -> if b
-                                      then Left $ Success mdule
-                                      else Right $ Failure "Contract not a token"
-                                    x -> Right err
-                                  _ -> Right err
-                                ) (Failure "This module does not exist on any chain") responses
-                let
-                  (failureEv, eventEv) = fanEither $ updated failureOrEventDyn
-
-                resultEv <- switchHold never eventEv
-                pure $ leftmost [failureEv, resultEv]
-
-              triggerEv inputEl = () <$ tag (current $ value inputEl) addClickEv
-            dmFung <- divClass "group flex-grow" $
-              fmap snd $ uiTextInputAsync "Enter token" True (Nothing, PopoverState_Disabled) (Just "" <$ addEv) triggerEv tokenValidator moduleListId
+            --TODO: add trigger
+            -- let triggerEv inputEl = () <$ tag (current $ value inputEl) addClickEv
+            dmFung <- divClass "group flex-grow" $ tokenInput model network chainToModuleMap $ current localListDyn
             addClickEv <- confirmButton (def & uiButtonCfg_class .~ "margin") "Add"
             pure dmFung
           eventEv <- networkView $ localListDyn <&> \ne -> do
@@ -168,7 +195,7 @@ inputToken model _ = do
           deleteToken (t NE.:| ts) token = t NE.:| filter (/= token) ts
           processUserAction action tokens = either (deleteToken tokens) (addToken tokens) action
           addEv = fmapMaybe id $ updated dmFung
-        localListDyn <- foldDyn processUserAction initialTokens $ leftmost
+        localListDyn::_ <- foldDyn processUserAction initialTokens $ leftmost
           [ Left <$> deleteEv
           , Right <$> addEv
           ]
