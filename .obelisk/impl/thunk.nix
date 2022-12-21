291d48e30e2606e9ab820cc8108982b8447fafbe
John Wiegley <johnw@newartisans.com>
2022-12-21 12:23:31 -0800

    Change needed to build on macOS 10.15

--- .obelisk/impl/thunk.nix
+++ .obelisk/impl/thunk.nix
@@ -5,5 +5,11 @@ let fetch = { private ? false, fetchSubmodules ? false, owner, repo, rev, sha256
   } else (import <nixpkgs> {}).fetchFromGitHub {
     inherit owner repo rev sha256 fetchSubmodules private;
   };
-  json = builtins.fromJSON (builtins.readFile ./github.json);
+  json =
+    builtins.fromJSON
+      (builtins.readFile
+         (if (builtins.currentSystem == "x86_64-darwin") ||
+             (builtins.currentSystem == "aarch64-darwin")
+          then ./github-mac.json
+          else ./github.json));
 in fetch json
